29e1928349c69b23cec09bcfa82b0a510f0ea758
Arseniy Oguzov <arseniyoguzov@gmail.com>
2021-08-05 12:30:46 +0300

    Create contains-duplicate.js

---
+++ contains-duplicate.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+const containsDuplicate = function(nums) {
+    nums.sort((a,b) => a - b);
+    
+    for(let i = 1; i<nums.length; i++) {
+        if(nums[i] === nums[i-1]) {
+            return true;
+        }
+    }
+    return false;
+};
